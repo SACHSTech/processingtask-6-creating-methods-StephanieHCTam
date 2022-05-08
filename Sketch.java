614a1fd6b5fbbab6ac6002b3bc52b81d331c827d
StephanieHCTam <hoiching.tam23@ycdsbk12.ca>
2022-05-08 13:51:40 -0400

    Completed assignment

--- Sketch.java
+++ Sketch.java
@@ -1,12 +1,11 @@
 import processing.core.PApplet;
 
 /**
- * Drawing objects using parameters from methods.
+ * A program that draws a bird using methods with parameters and return values.
  * @author: Stephanie Tam
  */
 
 public class Sketch extends PApplet {
-	
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -21,7 +20,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(173, 230, 255);
   }
 
   /**
@@ -29,13 +28,131 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Draw body
+    drawBody(200, 200, 260, 230);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    // Draw stomach
+    drawStomach(200, 265, 160, 100);
+
+    // Draw right wing
+    drawRWings(320, 190, 320, 240, 385, 190);
+
+    // Draw left wing
+    drawLWings(80, 190, 80, 240, 15, 190);
+
+    // Draw beak
+    drawBeak(170, 180, 200, 230, 230, 180);
+
+    // Draw left eye
+    drawLEye(185, 170, 20, 30);
+
+    // Draw right eye
+    drawREye(215, 170, 20, 30);
   }
   
   // define other methods down here.
+
+  /**
+   * Draws the bird's body
+   * 
+   * @param circleX x-coordinate of the ellipse
+   * @param circleY y-coordinate of the ellipse
+   * @param circleWidth width of the ellipse
+   * @param circleHeight height of the ellipse
+   */
+
+  public void drawBody(float circleX, float circleY, float circleWidth, float circleHeight) {
+    fill(242, 22, 84);
+    ellipse(circleX, circleY, circleWidth, circleHeight);
+  }
+
+  /**
+   * Draws the bird's stomach
+   * 
+   * @param circleX x-coordinate of the ellipse
+   * @param circleY y-coordinate of the ellipse
+   * @param circleWidth width of the ellipse
+   * @param circleHeight height of the ellipse
+   */
+
+  public void drawStomach(float circleX, float circleY, float circleWidth, float circleHeight) {
+    fill(173, 122, 245);
+    ellipse(circleX, circleY, circleWidth, circleHeight);
+  }
+
+  /**
+   * Draws the bird's right wing
+   * 
+   * @param triangleX1 x-coordinate of the first point
+   * @param triangleY1 y-coordinate of the first point
+   * @param triangleX2 x-coordinate of the second point
+   * @param triangleY2 y-coordinate of the second point
+   * @param triangleX3 x-coordinate of the third point
+   * @param triangleY3 y-coordinate of the third point
+   */
+
+  public void drawRWings(float triangleX1, float triangleY1, float triangleX2, float triangleY2, float triangleX3, float triangleY3) {
+    fill(92, 0, 222);
+    triangle(triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3);
+  }
+
+  /**
+   * Draws the bird's left wing
+   * 
+   * @param triangleX1 x-coordinate of the first point
+   * @param triangleY1 y-coordinate of the first point
+   * @param triangleX2 x-coordinate of the second point
+   * @param triangleY2 y-coordinate of the second point
+   * @param triangleX3 x-coordinate of the third point
+   * @param triangleY3 y-coordinate of the third point
+   */
+
+  public void drawLWings(float triangleX1, float triangleY1, float triangleX2, float triangleY2, float triangleX3, float triangleY3) {
+    fill(92, 0, 222);
+    triangle(triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3);
+  }
+
+  /**
+   * Draws the bird's beak
+   * 
+   * @param triangleX1 x-coordinate of the first point
+   * @param triangleY1 y-coordinate of the first point
+   * @param triangleX2 x-coordinate of the second point
+   * @param triangleY2 y-coordinate of the second point
+   * @param triangleX3 x-coordinate of the third point
+   * @param triangleY3 y-coordinate of the third point
+   */
+  
+  public void drawBeak(float triangleX1, float triangleY1, float triangleX2, float triangleY2, float triangleX3, float triangleY3) {
+    fill(255, 233, 38);
+    triangle(triangleX1, triangleY1, triangleX2, triangleY2, triangleX3, triangleY3);
+  }
+
+  /**
+   * Draws the bird's left eye
+   * 
+   * @param circleX x-coordinate of the ellipse
+   * @param circleY y-coordinate of the ellipse
+   * @param circleWidth width of the ellipse
+   * @param circleHeight height of the ellipse
+   */
+
+  public void drawLEye(float circleX, float circleY, float circleWidth, float circleHeight) {
+    fill(24, 0, 102);
+    ellipse(circleX, circleY, circleWidth, circleHeight);
+  }
+
+  /**
+   * Draws the bird's right eye
+   * 
+   * @param circleX x-coordinate of the ellipse
+   * @param circleY y-coordinate of the ellipse
+   * @param circleWidth width of the ellipse
+   * @param circleHeight height of the ellipse
+   */
+
+  public void drawREye(float circleX, float circleY, float circleWidth, float circleHeight) {
+    fill(24, 0, 102);
+    ellipse(circleX, circleY, circleWidth, circleHeight);
+  }
 }
